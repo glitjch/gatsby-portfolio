c8b83296749a456978d1b9e62edcb5bc803b047f
TJ Phan <t.jamesphan@gmail.com>
2022-05-11 23:10:20 -0700

    Add About page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => {
+  return (
+    <main>
+    <title>About TJ</title>
+    <Link to="/">Return Home</Link>
+    <h1>The Story So Far</h1>
+    <p>Ten years in the medical field as an optician, TJ gained insight on personable customer service, listening beyond standard client needs, as people trusted him enough to share their personal passions and challenges. He increasingly realized that helping people is his calling, but his service to others was limited, believing his untapped creative potential could support others so much more. After years of plain medical software applications, countless patient stories that could’ve used solutions by design, TJ took a chance with coding as the outlet to influence change. There was no turning back.</p>
+  </main>
+  );
+};
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import * as React from "react"
+import { Link } from 'gatsby';
 
-// markup
+// COMPONENT
 const IndexPage = () => {
+
+  // VIEW
   return (
     <main>
       <title>TJ Phan - Software Engineer</title>
+      <Link to='about'>About</Link>
       <h1>Hi, I'm TJ!</h1>
       <p>Canadian Software Engineer from Coquitlam, British Columbia.</p>
       <p>I'm passionate about building software services that help communities to thrive and support pursuits of personal wellbeing.</p>
       <p>ESG-focused (Environment-Social-Governance).</p>
-      
     </main>
   )
 }
