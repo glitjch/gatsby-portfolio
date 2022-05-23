271a9f239314b1573c5039c676e7ec810329b39c
TJ Phan <t.jamesphan@gmail.com>
2022-05-23 12:26:34 -0700

    Add social icons and its components

--- src/components/social.js
+++ src/components/social.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import * as classes from './styles/social.module.css';
 
+import { StaticImage } from "gatsby-plugin-image"
+
+import github from '../images/github.svg';
+import linkedin from '../images/linkedin.svg';
+import dev from '../images/dev.svg';
+import angel from '../images/angel.svg';
+import twitter from '../images/twitter.svg';
+
+// COMPONENT
 const Social = () => {
   return (
     <ul className={classes.social}>
-      <img src={"#"} alt="social"/>
-      <img src={"#"} alt="social"/>
-      <img src={"#"} alt="social"/>
-      <img src={"#"} alt="social"/>
+      <img src={github} alt="social"/>
+      <img src={linkedin} alt="social"/>
+      <StaticImage className={classes.hi} src="twitter.svg" alt='social'/>
+      <img src={dev} alt="social"/>
+      <img src={angel} alt="social"/>
+      <img src={twitter} alt="social"/>
     </ul>
   )
 }
