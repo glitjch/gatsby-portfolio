f603d7c3bf5c3a93c2f9c954347914e587aa903d
TJ Phan <t.jamesphan@gmail.com>
2022-05-23 23:12:57 -0700

    Update Footer component with layout and styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,24 @@
 import React from 'react'
+
 import * as classes from './styles/footer.module.css';
+
+import logo from '../images/nav-logo.svg';
+
 const Footer = () => {
   return (
     <footer className={classes.footer}>
-      Footer
+      
+      <section>
+        <img src={logo} alt='logo for tj phan'/>
+        <ul>
+          <li>Twitter</li>
+          <li>LinkedIn</li>
+          <li>Github</li>
+          <li>Dev</li>
+          <li>AngelList</li>
+        </ul>
+        <p>© 2022 TJ Phan, designed and built by. Spread the Love.</p>
+      </section>
 
     </footer >
   );
