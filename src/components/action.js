8ade29776311ef399dbf83171d1c21f08db09557
TJ Phan <t.jamesphan@gmail.com>
2022-05-23 22:43:36 -0700

    Update content of Action component

--- src/components/action.js
+++ src/components/action.js
@@ -4,11 +4,10 @@ import * as classes from './styles/action.module.css';
 const Action = () => {
   return (
     <section className={classes.action}>
-      <h2>Say Hello</h2>
-      <h3>Say Hello</h3>
-      <h4>Say Hello</h4>
-      <p>Canadian Software Engineer from Coquitlam, British Columbia.</p>
-      <p>I'm passionate about building software services that help communities to thrive and support pursuits of personal wellbeing.</p>
+      <button type='button'>
+        <h2>Say Hello</h2>  
+      </button>
+      <p>Proud Canadian Software Engineer from Coquitlam, British Columbia.</p>
       <p>ESG-focused (Environment-Social-Governance).</p>
     </section>
   )
