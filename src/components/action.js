b52efc7768f72964ed1436ed17c65367a787e0fc
TJ Phan <t.jamesphan@gmail.com>
2022-05-23 22:30:59 -0700

    Delete component folder and replaced with backup copy due to error commit of too large file

---
+++ src/components/action.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as classes from './styles/action.module.css';
+
+const Action = () => {
+  return (
+    <section className={classes.action}>
+      <h2>Say Hello</h2>
+      <h3>Say Hello</h3>
+      <h4>Say Hello</h4>
+      <p>Canadian Software Engineer from Coquitlam, British Columbia.</p>
+      <p>I'm passionate about building software services that help communities to thrive and support pursuits of personal wellbeing.</p>
+      <p>ESG-focused (Environment-Social-Governance).</p>
+    </section>
+  )
+}
+
+export default Action;
--- src/components/work.js
+++ src/components/work.js
@@ -5,12 +5,17 @@ import * as classes from './styles/work.module.css';
 import Project from './project';
 
 import Snappy from '../images/snappy.gif';
+import ProxyRitual from '../images/proxy-ritual.gif';
+import PawFive from '../images/pawfive.gif';
+import Scheduler from '../images/schedule-interviewer.gif';
+import Tweetr from '../images/tweetr.gif';
+
 
 // COMPONENT
 const Work = () => {
   return (
     <div className={classes.work}>
-      <h2>What I've built so far</h2>
+      <h2>Case Studies</h2>
       <section>
         <Project 
           projectTitle={"Snappy"} 
@@ -19,9 +24,27 @@ const Work = () => {
           projectDesc={"Want to land that valuable connection with that boss/star/career crush in the elevator? Open the    app, sprinkle a bit about yourself and let the trained ai to creat your personal elevator pitch, fast!"}
           projectStack={"ReactJS  ExpressJS  Components-CSS  Openai-API"}
           />
-        <Project projectTitle={"Proxy Ritual"}/>
-        <Project projectTitle={"Snappy"}/>
-        <Project projectTitle={"Snappy"}/>
+        <Project 
+          projectTitle={"Proxy Ritual"}
+          projectImage={ProxyRitual} 
+          projectId={"project_02"}
+          projectDesc={"Big game time is lost when there aren't enough duplicate cards in Arkham Horror LCG, a multiplayer campaigning card game. This app finds your deck, formats them to be ready for printing. No more hand-written proxying and more time to play."}
+          projectStack={"ReactJS Typescript Sass Axios PrintJS"}
+          />
+        <Project 
+          projectTitle={"Interview Scheduler"}
+          projectImage={Scheduler} 
+          projectId={"project_03"}
+          projectDesc={"Designed for busy appointment schedulers, the Scheduler helps experts and novices find their booking routines made easily managed with a few clicks."}
+          projectStack={"ReachJS Axios Cypress Storybook Jest"}
+          />
+        <Project 
+          projectTitle={"Tweetr"}
+          projectImage={Tweetr} 
+          projectId={"project_04"}
+          projectDesc={"Let the world hear your thoughts! Join other users in creating easy snippets of ideas or opinions and browse them through threads. CSS-focused project."}
+          projectStack={"ExpressJS Node-5.10 Chance Body-Parser Jquery"}
+          />
       </section>
     </div>
   )
