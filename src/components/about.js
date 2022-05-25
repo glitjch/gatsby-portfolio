a72a2e9260de439e9fd27b19b0021034eb733ae6
TJ Phan <t.jamesphan@gmail.com>
2022-05-24 21:02:47 -0700

    Add svg images to about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 import * as classes from "./styles/about.module.css";
+import TechJS from '../images/tech-js.svg';
+import TechReact from '../images/tech-react.svg';
+import TechNodejs from '../images/tech-nodejs.svg';
+import TechGithub from '../images/tech-github.svg';
+import TechPostgresql from '../images/tech-postgresql.svg';
+import TechRuby from '../images/tech-ruby.svg';
+import TechTypescript from '../images/tech-typescript.svg';
+import TechHtml from '../images/tech-html.svg';
+import TechCss from '../images/tech-css.svg';
+
+
 
 const About = () => {
   return (
@@ -10,13 +21,13 @@ const About = () => {
       <p>I architecture web applications with human factor. I work with all levels of complexity to produce simplicity at its best, whether that's designing your frontend web presence or structuring your server performance. Coding is an online wonderland of possibilities, and helping others realize theirs is my worldly passion.</p>
       <p>Say hi, I'm super friendly</p>
       <ul>
-        <img alt='tech'/>
-        <img alt='tech'/>
-        <img alt='tech'/>
-        <img alt='tech'/>
-        <img alt='tech'/>
-        <img alt='tech'/>
-        <img alt='tech'/>
+        <img src={TechJS} alt='tech'/>
+        <img src={TechReact} alt='tech'/>
+        <img src={TechNodejs} alt='tech'/>
+        <img src={TechGithub} alt='tech'/>
+        <img src={TechPostgresql} alt='tech'/>
+        <img src={TechTypescript} alt='tech'/>
+        <img src={TechRuby} alt='tech'/>
       </ul>
     </div>
   );
