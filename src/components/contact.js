cffd57e5331361199e2fd6304ea39243c55bfdde
TJ Phan <t.jamesphan@gmail.com>
2022-05-26 15:38:13 -0700

    Update contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,6 +8,7 @@ import CloseIcon from './closeIcon';
 
 // COMPONENT
 const Contact = () => {
+  const [ status, setStatus ] = useState("Submit") 
   const [ name, setName ] = useState("");
   const [ email, setEmail ] = useState("");
   const [ message, setMessage ] = useState("");
@@ -24,13 +25,33 @@ const Contact = () => {
   };
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setStatus("Sending...")
     if ([name, email, message].some((val) => val.length === 0)) return;
     if (name.length < 3) return;
     if (email.length < 6) return;
     if (message.length < 10) return;
 
+    const { name, email, message } = e.target.elements;
+
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+
+    let response = await fetch("http://localhost:8000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
 
     setName("");
     setEmail("");
@@ -44,12 +65,14 @@ const Contact = () => {
         <CloseIcon />
       </div>
       <h2>Contact</h2>
-      <label htmlFor='name'>Add your name</label>
-      <input name='name' value={name} onChange={handleName}/>
-      <label htmlFor='email'>then your email</label>
-      <input name='email' value={email} onChange={handleEmail}/>
-      <label htmlFor='message'>finally your message</label>
-      <textarea name='message' value={message} onChange={handleMessage}/>
+      <label htmlFor='name'>Add your name*</label>
+      <input name='name' type={"text"} id={"name"} value={name} onChange={handleName} required/>
+      <label htmlFor='email'>then your email*</label>
+      <input name='email' type={"email"} id={"email"} value={email} onChange={handleEmail} required/>
+      <label htmlFor='message'>finally your message*</label>
+      <textarea name='message' value={message} id={"message"} onChange={handleMessage} 
+      placeholder={"Project inquiries, or even a tea-ffee."}
+      required/>
       <button type='submit'>send</button>
     </form>
   );
